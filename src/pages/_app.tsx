dc42b1192082f86bc1fa059f92ee1c5e4e8eb020
gabrielarroyoc <gabrielarroyoc18@gmail.com>
2023-01-26 01:02:12 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 import { AppProps } from "next/app";
+import logoImg from "../assets/logo.svg";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  <div>
-    return <Component {...pageProps} />
-  </div>;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
